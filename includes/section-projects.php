78f1f56926cdfdc21d2a4c992471b6cbbd93a7f7
Gustav Cederlund <70195738+Cederlund1999@users.noreply.github.com>
2021-11-01 15:22:12 +0100

    test with bootstrap cards

--- includes/section-projects.php
+++ includes/section-projects.php
@@ -1,16 +1,31 @@
 <?php
 get_header();
-?>
-    <div>
-        <?php if ( have_posts() ) : while ( have_posts() ) : the_post();
-            echo the_title();
-            echo the_excerpt();
 
+?>
+<div>
+    <div class="projectPosts">
+        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+            <?php $image = get_field('image');
+            $backgroundImage = $image['sizes']['large']; ?>
+        <div class="projectCards">
+            <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="<?php echo $backgroundImage?>" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title"><?php echo the_title();?></h5>
+                    <p class="card-text"><?php echo the_excerpt();?></p>
+                    <a href="<?php echo the_permalink();?>" class="btn btn-primary">Go to project</a>
+                </div>
+            </div>
+        </div>
+
+<?php
         endwhile; else: ?>
             <p>Sorry, no posts found.<p></p>
         <?php endif; ?>
     </div>
+</div>
 
 <?php
 get_footer();
 ?>
+
