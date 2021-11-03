ef2431a97faefad96e877456f0956eebafad3f0d
Gustav Cederlund <70195738+Cederlund1999@users.noreply.github.com>
2021-11-03 14:35:17 +0100

    front-page input data from wordpress admin

--- front-page.php
+++ front-page.php
@@ -1,4 +1,14 @@
 <?php
+$title = get_field('title');
+$desc = get_field('desc');
+$getImage = get_field('image');
+$image = $getImage['sizes']['large'];
+$link = get_field('imageLink');
 get_header();
-echo "hello world";
+?>
+<h1><?php echo $title;?></h1>
+<div><?php echo $desc;?></div>
+<a href="<?php echo $link;?>"><img src ="<?php echo $image;?>"></a>
+
+<?php
 get_footer();
